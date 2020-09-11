6868f1ed8c998893ab3dc9e48f5bd18f8153ef1c
IsaacGHoward <howardisaac100@gmail.com>
2020-09-11 16:33:34 -0700

    closure.js complete

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,17 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+/*
+nestedFunction can access the variable 'internal' because iternal is one level above the scope of nestedFunction, which is inside of the function that contains nestedFunction (myFunction). Since variables from outer scopes can be used in inner scopes, this variable can be accessed from inside nestedFunction.
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num){
+  if(num === 1)
+    return 1;
+  else 
+    return summation(num-1) + num;
+}
+console.log(summation(4));
